8a11ac97c2839ba15ecc0f71f5c0fbaee5c742c5
mythili63 <148697056+mythili63@users.noreply.github.com>
2023-10-22 07:24:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('.menu')
+     const menulist = document.querySelector('nav ul')
+     menu.addEventListener('click',() => {
+        menulist.classList.toggle('showmenu')
+     })   
